bfa147836b6f0d767f4f84df32897129ecc42e02
Mostafa <m.sorkhi@ut.ac.ir>
2022-09-03 23:32:17 +0430

    This is for importing Code

---
+++ Gussing_Game.py
@@ -0,0 +1,22 @@
+N = 'a'
+a = 0
+b = 99
+from msvcrt import kbhit
+import random
+hads = random.randint(a,b)
+
+print (hads)
+while N != 'd':
+     N = input ('hadse sys chetor ast ?'    )
+     if N == 'b': 
+         b = hads
+         hads = random.randint(a,b)
+         print (hads)
+     elif N == 'k':
+         a = hads
+         hads = random.randint(a,b)
+         print (hads)
+     elif N == 'd': 
+         break   
+print ( 'hadse shoma' , hads , 'ast')
+
